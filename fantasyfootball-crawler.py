7aeab24cbb4ad047b7a3a815f73ef6371ed9ac8a
William Wang <william.w@columbia.edu>
2023-02-19 23:01:11 -0500

    Added dictionary and more generalization

--- fantasyfootball-crawler.py
+++ fantasyfootball-crawler.py
@@ -1,31 +1,38 @@
 import bs4 as bs
 import pandas as pd
 
-def get_tables(url):
+# To make this generalized across all websites, there is a searchTerm 
+# associated with the HTML wrappers of the website.
+def get_tables(url, searchTerm):
     soup = bs.BeautifulSoup(url, 'lxml')
-    # mostly all of the statistics sites are built on <table_wrapper> tags, so this will retrieve them
-    table = soup.find_all('table') 
-    dataframe = pd.read_html(str(table))
+    table = soup.find_all(searchTerm) 
+    dataframe = pd.read_html(str(searchTerm))
     return dataframe
 
+# This function gets the dataframe from a given table.
 def read_tables(table, index):
     df = pd.read_html(str(table))[index]
     return df
 
-# this is a generalized parsing function, but I will make specific ones for each site because each has different format
-def parse(url):
-    df = get_tables(url)
+# This function parses a URL, searchTerm (HTML wrapper), and prints the dataframe contents.
+def parse_print(url, searchTerm):
+    df = get_tables(url, searchTerm)
     for i in range(len(df)):
         curTable = read_tables(df, i)
         for j in range(len(curTable)):
             print(curTable[j])
 
-urlList = ["https://www.pro-football-reference.com/years/2022/advanced.htm", 
-"https://fantasydata.com/nfl/fantasy-football-leaders", "https://www.pff.com/fantasy/stats", 
-"https://nextgenstats.nfl.com/", "https://theanalyst.com/na/2021/09/nfl-stats-zone/"]
+# A dictionary containing the URL and search term, with the names as keys.
+urlSearchDict = {
+    pff : ["https://www.pff.com/fantasy/stats", 'kyber-table-body___rows'],
+    fantasyData : ["https://fantasydata.com/nfl/fantasy-football-leaders", 'stats_grid'],
+    pfr : ["https://www.pro-football-reference.com/years/2022/advanced.htm", 'tbody'],
+    nextGen : ["https://nextgenstats.nfl.com/stats/top-plays/fastest-ball-carriers", 'el-table__body-wrapper'],
+    analyst : ["https://theanalyst.com/na/2021/09/nfl-stats-zone/", 'colgroup']
+}
 
-for i in range(len(urlList)):
-    parse(urlList[i])
+for key in urlSearchDict.keys():
+    parse_print(urlSearchDict.get(key)[0], urlSearchDict.get(key)[1])
 
 
 
