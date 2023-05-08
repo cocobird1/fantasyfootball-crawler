24bdfe4b68e68904b291fa3abf6c9d2c358cc4f4
William Wang <william.w@columbia.edu>
2023-05-08 00:00:58 -0400

    Deprecated a lot of useless functions, and updated documentation and added signficantly more testing.

--- fantasyfootball-crawler/tests/test_all.py
+++ fantasyfootball-crawler/tests/test_all.py
@@ -3,54 +3,93 @@ from ff_functions import (
     getRushData,
     getRecData,
     getScoringData,
-    getResults,
     getAggregateTopPlayers,
-    read_pff,
-    read_dict,
-    soupInit,
 )
 
 
-def test_soup():
-    assert soupInit("https://fantasydata.com/nfl/fantasy-football-leaders") is not None
-
-
-def test_results():
-    assert getResults("a", True, soupInit("https://fantasydata.com/nfl/fantasy-football-leaders")) is not None
-
-
-def test_getTopPlayers():
-    soup = soupInit("https://fantasydata.com/nfl/fantasy-football-leaders")
-    assert getAggregateTopPlayers(getResults("a", True, soup)) is not None
-
-
-def test_getQBData():
+def test_getQBData1():
     assert getQBData(["QBR", "Int"]) is not None
 
 
-def test_getRushData():
+def test_getQBData2():
+    assert getQBData(["QBR", "Int", "Age"]) is not None
+
+
+def test_getQBData3():
+    assert getQBData([]) is None
+
+
+def test_getQBData4():
+    assert getQBData(["lol"]) is None
+
+
+def test_getRushData1():
     assert getRushData(["Y/A", "Yds"]) is not None
 
 
-def test_getRecData():
+def test_getRushData2():
+    assert getRushData(["Y/A", "Yds", "Att", "G"]) is not None
+
+
+def test_getRushData3():
+    assert getRushData([]) is None
+
+
+def test_getRushData4():
+    assert getRushData(["lol"]) is None
+
+
+def test_getRecData1():
     assert getRecData(["Rec", "Yds"]) is not None
 
 
-def test_getScoringData():
+def test_getRecData2():
+    assert getRecData(["Rec", "Tgt", "TD"]) is not None
+
+
+def test_getRecData3():
+    assert getRecData([]) is None
+
+
+def test_getRecData4():
+    assert getRecData(["lol"]) is None
+
+
+def test_getScoringData1():
+    assert getScoringData("QB") is not None
+
+
+def test_getScoringData2():
+    assert getScoringData("RB") is not None
+
+
+def test_getScoringData3():
     assert getScoringData("WR") is not None
 
 
-def test_pff_empty():
-    assert len(read_pff()) != 0
+def test_getScoringData4():
+    assert getScoringData("K") is not None
 
 
-def test_read_dict():
-    assert len(read_dict()) != 0
+def test_getScoringData5():
+    assert getScoringData("pos") is None
+
+
+def test_getScoringData6():
+    assert getScoringData("") is None
+
+
+def test_getTopPlayers():
+    assert getAggregateTopPlayers() is not None
 
 
 # Integration tests
 def integrationTest1():
-    soup = soupInit("https://fantasydata.com/nfl/fantasy-football-leaders")
-    results = getResults("a", True, soup)
-    ret = getAggregateTopPlayers(results)
+    dfQB = getQBData(["QBR"])
+    dfRush = getRushData(["Y/A"])
+    dfRec = getRecData(["Rec"])
+    ret = getAggregateTopPlayers()
     assert ret is not None
+    assert dfQB is not None
+    assert dfRush is not None
+    assert dfRec is not None
